ccf198b17003bbffffcc6b198ad97af16da476b3
MariellaP123 <mmp344@gmail.com>
2014-12-15 17:57:31 -0500

    organize the urls so that they can identify each other. ultimately the goal is for the urls to direct me to app/about and say hello. Test-pass

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
+
+def about(request):
+    return HttpResponse ("hello")
