f57a17bb42d6f6a07f0f610543c1ae6527879f55
MariellaP123 <mmp344@gmail.com>
2014-12-15 20:41:14 -0500

    we are combining strings and data to make combined sentences in our views to showcase the interaction between the data and the HTML. Test, pass!

--- app/urls.py
+++ app/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     #PART 2: we have arived here from the big prent folder (adding_data_app/urls) and the line that said: url(r'^app/', include('app.urls').  
     #in this line, our app is telling you, you have differnt things that you can do when you have arrived in this step.  In this instance, our choices are about.  
 
-    #our code is telling us, "oh, you want to go to about, please go to the vies.py file and follow the file that is called about."  
+    #our code is telling us, "oh, you want to go to about, please go to the views.py file and follow the file that is called about."  
 )
--- app/views.py
+++ app/views.py
@@ -5,21 +5,13 @@ from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 
 def about(request):
-#remember, the function 'def' is like the secretary that asks you, "hi, welcome, who are you here to see?  In this case, our function is ABOUT... and we are going to tell you what about is going to do below... REMEMBER, indents are important!"
-
-    the_message = "Hello. I am all ABOUT that life"
-    #PART 4.2: You are now defining the function about to tell you something, this is similar to what we did previously, but this time, because we have multiple functions, the way it will be written down will be a tad different. 
-
+    first_name = "Mariella" 
+    last_name = "Paulino" 
+    the_message = "Hello! My name is "+first_name+" "+last_name+" "+"and I am looking for an awesome software development position!!!"
     return HttpResponse (the_message)
+    #PART 5: This is where all the information gets stored.  In other words, this is where all the information gets stored in a way that all the data gets translated in a language that HTML can use.  
 
-    #PART 3: In this part you have moved all the way into the building, up the stairs into the floor, into the office, and you are now wanting to speak and chat with someone very specific.  You want to speak to someone called Hello, and this is what you will see when you open the browser, a page that says hello" 
-
-    #--> test the page by going to your server and typing... 8000/app/about/... your page should just be a page that says 'hello' -TEST PASS move on! 
-    
-    '''return HttpResponse ("hello")'''
-    #PART 4.1: DELETE... you are no longer using this because you are moving on a multiple "people" within the page  
 
 def index (request):
-    #PART 4.3 we are bringing a new function to the page, or a new "person"  In this case, our new person is going to be Index.  We want the secretary to guide us to index
-    the_second_message = "INDEX is all obsessed about his windows. He carries everywhere, what a weird dude!"
+    the_second_message = "INDEX is all obsessed about his windows. He carries Windex everywhere, what a weird dude!"
     return HttpResponse (the_second_message)
