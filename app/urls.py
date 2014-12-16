c688961380a6f2112077881c1bee9112139ba3f5
MariellaP123 <mmp344@gmail.com>
2014-12-15 19:52:58 -0500

    we are ensuring the functions ar emoving along seamlessly and we have created a new fuction within the app, in this case, another person, to ensure the site is moving.  In our example, the app is moving from the big building, to the floor, to the office, and it is interacting with the secretary and asking for two different people, About (who is all ABOUT that life) and Index (who is obsessed with Windex for his windows.)

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,9 @@ from app import views
 urlpatterns = patterns('',
     url(r'^about/$', views.about, name='about'),
     #"Hi, you are in the second floor of the big building, which office would you like to go into?
-
+    url(r'^index/$', views.index, name='index')
+    #PART 4.4:  We created a index part in the views.py folder within the app, now we have to also create a guide to tell the site, hey, when you see someone asking for index, rmemeber to take them to the index function within the views page!
+    
     #PART 2: we have arived here from the big prent folder (adding_data_app/urls) and the line that said: url(r'^app/', include('app.urls').  
     #in this line, our app is telling you, you have differnt things that you can do when you have arrived in this step.  In this instance, our choices are about.  
 
--- app/views.py
+++ app/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+ 
 
 # Create your views here.
 
 def about(request):
-    #'Hi you are in room number 123, who would you like to speak to?'
+#remember, the function 'def' is like the secretary that asks you, "hi, welcome, who are you here to see?  In this case, our function is ABOUT... and we are going to tell you what about is going to do below... REMEMBER, indents are important!"
 
-    #PART 3: In this part you have moved all the way into the building, up the stairs into the floor, into the office, and you are now wanting to speak and chat with someone very specific.  You want to speak to someone called Hello, and this is what you will see when you open the browser, a page that says hello"
-    return HttpResponse ("hello")
+    the_message = "Hello. I am all ABOUT that life"
+    #PART 4.2: You are now defining the function about to tell you something, this is similar to what we did previously, but this time, because we have multiple functions, the way it will be written down will be a tad different. 
+
+    return HttpResponse (the_message)
+
+    #PART 3: In this part you have moved all the way into the building, up the stairs into the floor, into the office, and you are now wanting to speak and chat with someone very specific.  You want to speak to someone called Hello, and this is what you will see when you open the browser, a page that says hello" 
+
+    #--> test the page by going to your server and typing... 8000/app/about/... your page should just be a page that says 'hello' -TEST PASS move on! 
+    
+    '''return HttpResponse ("hello")'''
+    #PART 4.1: DELETE... you are no longer using this because you are moving on a multiple "people" within the page  
+
+def index (request):
+    #PART 4.3 we are bringing a new function to the page, or a new "person"  In this case, our new person is going to be Index.  We want the secretary to guide us to index
+    the_second_message = "INDEX is all obsessed about his windows. He carries everywhere, what a weird dude!"
+    return HttpResponse (the_second_message)
