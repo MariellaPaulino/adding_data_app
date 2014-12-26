745cc16ce6b50fa464d14177c55b54825cd7dda2
MariellaP123 <mmp344@gmail.com>
2014-12-25 19:58:32 -0500

    we have added two new ways of adding data to our app which you can see in the video part 7 and 8.  Next we are going to add data throug a csv file, but before we do that, we want to change the views of the site to include unicode, the language that allows our objects to be able to be read in 'normal' language.  We are changing that little peice of code now.

--- app/models.py
+++ app/models.py
@@ -7,3 +7,7 @@ class Women_Warriors(models.Model):
     nationality = models.CharField(max_length=40)
     real_life = models.BooleanField(default=True)
     bio = models.TextField()
+
+    # PART 9: Objects exist in the mind of a computer.  "Computer language".  The computer doesn't understand that human lnaguage is useful.  Because it is not useful to it.  The computer understands 0s and 1s.  'Unicode' is the map that computer scientist developed to translate computer language to human text.  WHen you go on the admin site, before you put the line below, the site would list, the data you have put on the site as "Women_Warriors object"  After you have put this line in the site, the site lists these women by their name, since this is what you are telling the site to translate it to. 
+    def __unicode__(self):
+        return self.name
